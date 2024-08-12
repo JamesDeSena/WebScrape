599ccfbf17952f1380cd6adf660f661b44112854
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-08-12 18:03:19 +0800

    ds

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,13 +1,75 @@
 import React from "react";
-import  "./main.css";
-import {ToastContainer} from "react-toastify";
+import "./main.css";
+import { ToastContainer } from "react-toastify";
 
-const Landing = () =>{
+const Landing = () => {
 
-    return(
-        <div>
-            <ToastContainer/> 
-            <h2>hello jello</h2>
+    return (
+        <div className="land">
+            <ToastContainer />
+            <h3>LAST RETRIEVED: 2 MINS. AGO</h3>
+
+            <div className="articlecont"> 
+                <div className="articles">
+                    <div className="content">
+                        <h2>PNP eyes Quiboloy arrest inside Davao compound without violence</h2>
+                        <p>A Philippine National Police spokesperson admitted Friday arresting controversial televangelist Apollo Quiboloy inside his Kingdom of Jesus Christ compound in Davao City is not a simple matter. Police Brig. Gen. Nicolas Torre III, Police Regional Office (PRO)-Davao region chief, earlier ...</p>
+                    </div>
+                    <div className="content">
+                        <p className="retrieved">
+                        Retrieved 2 hours ago
+                        </p>
+                    </div>
+                </div>
+                <div className="articles">
+                    <div className="content">
+                        <h2>PNP eyes Quiboloy arrest inside Davao compound without violence</h2>
+                        <p>A Philippine National Police spokesperson admitted Friday arresting controversial televangelist Apollo Quiboloy inside his Kingdom of Jesus Christ compound in Davao City is not a simple matter. Police Brig. Gen. Nicolas Torre III, Police Regional Office (PRO)-Davao region chief, earlier ...</p>
+                    </div>
+                    <div className="content">
+                        <p className="retrieved">
+                        Retrieved 2 hours ago
+                        </p>
+                    </div>
+                </div>
+                <div className="articles">
+                    <div className="content">
+                        <h2>PNP eyes Quiboloy arrest inside Davao compound without violence</h2>
+                        <p>A Philippine National Police spokesperson admitted Friday arresting controversial televangelist Apollo Quiboloy inside his Kingdom of Jesus Christ compound in Davao City is not a simple matter. Police Brig. Gen. Nicolas Torre III, Police Regional Office (PRO)-Davao region chief, earlier ...</p>
+                    </div>
+                    <div className="content">
+                        <p className="retrieved">
+                        Retrieved 2 hours ago
+                        </p>
+                    </div>
+                </div>
+                <div className="articles">
+                    <div className="content">
+                        <h2>PNP eyes Quiboloy arrest inside Davao compound without violence</h2>
+                        <p>A Philippine National Police spokesperson admitted Friday arresting controversial televangelist Apollo Quiboloy inside his Kingdom of Jesus Christ compound in Davao City is not a simple matter. Police Brig. Gen. Nicolas Torre III, Police Regional Office (PRO)-Davao region chief, earlier ...</p>
+                    </div>
+                    <div className="content">
+                        <p className="retrieved">
+                        Retrieved 2 hours ago
+                        </p>
+                    </div>
+                </div>
+                <div className="articles">
+                    <div className="content">
+                        <h2>PNP eyes Quiboloy arrest inside Davao compound without violence</h2>
+                        <p>A Philippine National Police spokesperson admitted Friday arresting controversial televangelist Apollo Quiboloy inside his Kingdom of Jesus Christ compound in Davao City is not a simple matter. Police Brig. Gen. Nicolas Torre III, Police Regional Office (PRO)-Davao region chief, earlier ...</p>
+                    </div>
+                    <div className="content">
+                        <p className="retrieved">
+                        Retrieved 2 hours ago
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="pagination">
+                <button>FOPJFDKAHFJK</button>
+            </div>
         </div>
     );
 };
