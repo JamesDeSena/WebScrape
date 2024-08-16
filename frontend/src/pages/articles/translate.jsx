28bb7783fe5ba384905022bac5a955173f9153df
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-08-16 13:04:58 +0800

    dd

--- frontend/src/pages/articles/translate.jsx
+++ frontend/src/pages/articles/translate.jsx
@@ -1,1 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Translate = ({ textToTranslate }) => {
+    const [translatedText, setTranslatedText] = useState('');
+
+    useEffect(() => {
+        const fetchTranslation = async () => {
+            try {
+                const response = await axios.post('http://localhost:5000/translate', {
+                    text: textToTranslate
                 });
+                setTranslatedText(response.data.translation);
+            } catch (error) {
+                console.error('Error fetching translation:', error);
+            }
+        };
+
+        if (textToTranslate) {
+            fetchTranslation();
+        }
+    }, [textToTranslate]);
+
+    return (
+        <div>
+            <h2>Translated Article:</h2>
+            <div dangerouslySetInnerHTML={{ __html: translatedText }} />
+        </div>
+    );
+};
+
+export default Translate;
