899ab54957e8160f4c5b9904565fe5f43b0ca339
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-08-19 14:51:26 +0800

    ew

--- frontend/src/pages/tabs/manila-bulletin.jsx
+++ frontend/src/pages/tabs/manila-bulletin.jsx
@@ -34,16 +34,6 @@ const MBulletin = () => {
                     </div>
                 </div>
             </div>
-            <div className="pagination">
-                <div className="end">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>...</button>
-                </div>
-            </div>
         </div>
     );
 };
