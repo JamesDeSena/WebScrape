258ad7bc256b039006babd3f3aae734e9e7228c2
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-08-29 18:07:24 +0800

    sa

--- frontend/src/pages/articles/article.jsx
+++ frontend/src/pages/articles/article.jsx
@@ -13,10 +13,22 @@ const Article = () => {
         });
     };
 
-    // Convert content with line breaks to HTML with <br />
+    // Convert content with line breaks to HTML with <br /> IMPORTATNT
     const formatTextWithLineBreaks = (text) => {
         return text.split('\n').join('<br />');
     };
+    // useEffect(() => {
+    //     const fetchArticles = async () => {
+    //         try {
+    //             const response = await axios.get('http://localhost:8080/api/abs/get-page');
+    //         } catch (error) {
+    //             console.error("Error fetching articles:", error);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+    //     fetchArticles();
+    // }, []);
 
     return (
         <div>
@@ -32,41 +44,42 @@ const Article = () => {
                     </div>
                     <div className="contentsec" id="contentToCopy">
                         <h2 className="title">
-                            PNP eyes Quiboloy arrest inside Davao compound without violence
+                           {/* { article.title} */} LOREM IPSUM DOLOR SIT AMET
                         </h2>
-                        <hr />
                         <p className="dandr">
                             Date: August 29, 2024 | Retrieved: 5 secs. ago
                         </p>
+                        <hr />
+                        {/* IMPORTANT FUNCTION */}
                         <p className="content" dangerouslySetInnerHTML={{
                             __html: formatTextWithLineBreaks(
                                 `A Philippine National Police spokesperson admitted Friday arresting controversial televangelist Apollo Quiboloy inside his Kingdom of Jesus Christ compound in Davao City is not a simple matter.
 
-Police Brig. Gen. Nicolas Torre III, Police Regional Office (PRO)-Davao region chief, earlier said Quiboloy is still hiding inside the 30-hectare KOJC compound in Barangay Buhangin where he is shielded by KOJC members.
-
-Quiboloy is accused of sexual abuse and human trafficking and has a P10 million bounty for his arrest.
-
-Speaking to TeleRadyo Serbisyo, PNP spokesperson Col. Jean Fajardo said the police force is weighing its options on how to arrest Quiboloy after a previous attempt last June 10 led to a scuffle between law enforcers and KOJC members.
-
-PNP enters Quiboloy compound in Davao City
-
-“Hindi ganun ka-simple. Kung matatandaan mo, unang pinasok ‘yung KOJC compound sa Davao noong June 10 ay nagkaroon ng tensyon,” she explained.
-
-She said searching for Quiboloy inside the KOJC compound “could take hours if not days” because of the size of the estate.
-
-Police have also observed mass movement inside the compound, showing possible movement of KOJC supporters to protect the pastor.
-
-“It will take hours if not days to really search ‘yung napakalaking compound na ‘yun, kailangang pag-isipan ng maigi. This time around, gusto nating makasiguro na hindi mauulit ‘yung nangyari nung June 10,” she said.
-She reiterated the PNP’s appeal to Quiboloy to just surrender peacefully and avoid violence.
-The Court of Appeals has also issued a freeze order on Quiboloy’s 10 bank accounts, 7 real properties, 5 motor vehicles and an aircraft.
-CA freezes Quiboloy and KOJC bank accounts, properties`
+                                Police Brig. Gen. Nicolas Torre III, Police Regional Office (PRO)-Davao region chief, earlier said Quiboloy is still hiding inside the 30-hectare KOJC compound in Barangay Buhangin where he is shielded by KOJC members.
+                                
+                                Quiboloy is accused of sexual abuse and human trafficking and has a P10 million bounty for his arrest.
+                                
+                                Speaking to TeleRadyo Serbisyo, PNP spokesperson Col. Jean Fajardo said the police force is weighing its options on how to arrest Quiboloy after a previous attempt last June 10 led to a scuffle between law enforcers and KOJC members.
+                                
+                                PNP enters Quiboloy compound in Davao City
+                                
+                                “Hindi ganun ka-simple. Kung matatandaan mo, unang pinasok ‘yung KOJC compound sa Davao noong June 10 ay nagkaroon ng tensyon,” she explained.
+                                
+                                She said searching for Quiboloy inside the KOJC compound “could take hours if not days” because of the size of the estate.
+                                
+                                Police have also observed mass movement inside the compound, showing possible movement of KOJC supporters to protect the pastor.
+                                
+                                “It will take hours if not days to really search ‘yung napakalaking compound na ‘yun, kailangang pag-isipan ng maigi. This time around, gusto nating makasiguro na hindi mauulit ‘yung nangyari nung June 10,” she said.
+                                She reiterated the PNP’s appeal to Quiboloy to just surrender peacefully and avoid violence.
+                                The Court of Appeals has also issued a freeze order on Quiboloy’s 10 bank accounts, 7 real properties, 5 motor vehicles and an aircraft.
+                                CA freezes Quiboloy and KOJC bank accounts, properties`
                             )
                         }} />
                     </div>
-                    <div className="contentseco">
+                    {/* <div className="contentseco">
                         <button className="prev">&#171; PREVIOUS PAGE</button>
                         <button className="next">NEXT PAGE  &#187;</button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
