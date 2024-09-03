d42f7f4068d6e557cf1a1f6bf0e73ce2e19a4cfc
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-09-03 13:49:58 +0800

    Update article.jsx

--- frontend/src/pages/articles/article.jsx
+++ frontend/src/pages/articles/article.jsx
@@ -56,9 +56,6 @@ const Article = () => {
             <button className="return" onClick={() => navigate(-1)}>
               <IoReturnUpBackOutline /> RETURN
             </button>
-            <div className="breadcrumb">
-              CATEGORY: {name}
-            </div>
             <div className="groupbutton">
               <button className="copy" onClick={copyToClipboard}>
                 <FaCopy /> COPY
